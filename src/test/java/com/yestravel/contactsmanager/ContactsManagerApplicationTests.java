bf67e34e6d84953ccc40011f8b20e1b6f57d1289
isaac diez <isaac.diez@proton.me>
2025-08-11 18:44:49 +0200

    Feat <project>: initial main ContactsManagerApplication() class and test

---
+++ src/test/java/com/yestravel/contactsmanager/ContactsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yestravel.contactsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
