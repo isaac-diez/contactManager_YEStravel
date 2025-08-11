eeb577df1e10054177b4e5ff26e6ec3f614deaac
isaac diez <isaac.diez@proton.me>
2025-08-11 18:43:26 +0200

    Feat <service>: created IContactService() class and ContactServiceImpl()

---
+++ src/main/java/com/yestravel/contactsmanager/service/ContactServiceImpl.java
@@ -0,0 +1,45 @@
+package com.yestravel.contactsmanager.service;
+
+import com.yestravel.contactsmanager.model.Contact;
+import com.yestravel.contactsmanager.repo.ContactRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ContactServiceImpl implements IContactService {
+
+    @Autowired
+    private ContactRepo contactRepo;
+
+    @Override
+    public List<Contact> listContact() {
+
+        return contactRepo.findAll();
+
+    }
+
+    @Override
+    public Contact findContactById(Integer contactId) {
+
+        return contactRepo.findById(contactId).orElse(null);
+
+        //TODO: create exception
+
+    }
+
+    @Override
+    public void saveContact(Contact contact) {
+
+        contactRepo.save(contact);
+
+    }
+
+    @Override
+    public void deleteContact(Contact contact) {
+
+        contactRepo.delete(contact);
+
+    }
+}
