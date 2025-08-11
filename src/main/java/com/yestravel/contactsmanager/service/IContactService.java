c9dc6e071f3ab09eacfcff97aad391269849fb46
isaac diez <isaac.diez@proton.me>
2025-08-11 18:43:26 +0200

    Feat <service>: created IContactService() class and ContactServiceImpl()

---
+++ src/main/java/com/yestravel/contactsmanager/service/IContactService.java
@@ -0,0 +1,17 @@
+package com.yestravel.contactsmanager.service;
+
+import com.yestravel.contactsmanager.model.Contact;
+
+import java.util.List;
+
+public interface IContactService {
+
+    public List<Contact> listContact();
+
+    public Contact findContactById(Integer contactId);
+
+    public void saveContact(Contact contact);
+
+    public void deleteContact(Contact contact);
+
+}
