9dce5cd0a6278a15ad32fef1cddfc12a90234c78
isaac diez <isaac.diez@proton.me>
2025-10-20 11:15:03 +0200

    feat(exception): created UsernameAlreadyExistsException

---
+++ src/main/java/com/yestravel/contactsmanager/exception/UsernameAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.yestravel.contactsmanager.exception;
+
+public class UsernameAlreadyExistsException extends Throwable {
+    public UsernameAlreadyExistsException(String message) {
+        super(message);
+    }
+}
