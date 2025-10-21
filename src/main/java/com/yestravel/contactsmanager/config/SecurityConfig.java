046e3fc5d495d9e844a764604e117560c29da255
isaac diez <isaac.diez@proton.me>
2025-10-21 11:45:13 +0200

    refactor(security): refactored to work properly

--- src/main/java/com/yestravel/contactsmanager/config/SecurityConfig.java
+++ src/main/java/com/yestravel/contactsmanager/config/SecurityConfig.java
@@ -2,15 +2,18 @@ package com.yestravel.contactsmanager.config;
 
 import com.yestravel.contactsmanager.security.JwtAuthenticationFilter;
 import com.yestravel.contactsmanager.security.JwtService;
+import jakarta.servlet.*;
 import org.springframework.context.annotation.*;
 import org.springframework.security.authentication.*;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.*;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.*;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.*;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @Configuration
 public class SecurityConfig {
@@ -25,14 +28,20 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/contacts/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers(
+                                "/auth/**",
+                                "/login",
+                                "/register",
+                                "/css/**",
+                                "/js/**")
+                        .permitAll()
+                        .requestMatchers("/admin/**").hasAuthority("ROLE_ADMIN")
+                        .requestMatchers("/contacts/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
                         .anyRequest().authenticated()
                 )
-                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
-                .build();
+                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)                .build();
     }
 
     @Bean
