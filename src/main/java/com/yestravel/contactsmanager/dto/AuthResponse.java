2b708d73610507dc1651b044d9f1939d8fb851a7
isaac diez <isaac.diez@proton.me>
2025-10-21 11:37:47 +0200

    refactor(dto): moved from class to record

--- src/main/java/com/yestravel/contactsmanager/dto/AuthResponse.java
+++ src/main/java/com/yestravel/contactsmanager/dto/AuthResponse.java
@@ -3,8 +3,4 @@ package com.yestravel.contactsmanager.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@Getter
-@AllArgsConstructor
-public class AuthResponse {
-    private String token;
-}
+public record AuthResponse(String token) {}
