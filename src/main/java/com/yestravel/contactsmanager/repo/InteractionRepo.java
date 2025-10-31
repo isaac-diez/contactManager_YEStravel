817c025f40a09a4a0417dc3502607156975ddc3a
isaac diez <isaac.diez@proton.me>
2025-10-31 11:26:44 +0100

    feat(test): created repo

--- src/main/java/com/yestravel/contactsmanager/repo/InteractionRepo.java
+++ src/main/java/com/yestravel/contactsmanager/repo/InteractionRepo.java
@@ -1,6 +1,11 @@
 package com.yestravel.contactsmanager.repo;
 
 import com.yestravel.contactsmanager.model.Interaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface InteractionRepo extends JpaRepository<Interaction, Long> {}
+public interface InteractionRepo extends JpaRepository<Interaction, Long> {
+    Page<Interaction> findAllInteractionsByContact_Id(Long contactId, Pageable pageable);
+
+}
