e5d03a1c0788106a6da86435744a99041a6559e8
isaac diez <isaac.diez@proton.me>
2025-10-31 11:26:06 +0100

    feat(test): added createInteraction(), addInteraction() and findByContactId() returns a Page

--- src/main/java/com/yestravel/contactsmanager/service/InteractionService.java
+++ src/main/java/com/yestravel/contactsmanager/service/InteractionService.java
@@ -1,11 +1,15 @@
 package com.yestravel.contactsmanager.service;
 
 import com.yestravel.contactsmanager.model.Interaction;
+import com.yestravel.contactsmanager.model.InteractionType;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 public interface InteractionService {
-    public void saveInteraction(Interaction interaction);
+    public Interaction createInteraction(Interaction interaction);
+
+    public Interaction addInteraction(Long contactId, String username, InteractionType type, String description);
 
     public void deleteInteraction(Interaction interaction);
 
@@ -13,7 +17,7 @@ public interface InteractionService {
 
     public List<Interaction> findAllInteractions();
 
-    public List<Interaction> findAllInteractionsWithContactId(Integer contactId);
+    public Page<Interaction> findByContactId(Long contactId, int page, int size);
 
     public Interaction editInteraction (Interaction interaction);
 }
