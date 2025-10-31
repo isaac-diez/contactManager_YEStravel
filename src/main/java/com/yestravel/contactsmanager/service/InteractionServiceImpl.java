b8ef3b77ca1e7fbb36a54337f38db129694af3fe
isaac diez <isaac.diez@proton.me>
2025-10-31 11:25:43 +0100

    feat(test): added createInteraction(), addInteraction() and findByContactId() returns a Page

--- src/main/java/com/yestravel/contactsmanager/service/InteractionServiceImpl.java
+++ src/main/java/com/yestravel/contactsmanager/service/InteractionServiceImpl.java
@@ -1,23 +1,73 @@
 package com.yestravel.contactsmanager.service;
 
+import com.yestravel.contactsmanager.model.Contact;
 import com.yestravel.contactsmanager.model.Interaction;
+import com.yestravel.contactsmanager.model.InteractionType;
+import com.yestravel.contactsmanager.model.User;
+import com.yestravel.contactsmanager.repo.ContactRepo;
 import com.yestravel.contactsmanager.repo.InteractionRepo;
+import com.yestravel.contactsmanager.repo.UserRepo;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 public class InteractionServiceImpl implements InteractionService{
 
     private final InteractionRepo interactionRepo;
+    private final ContactRepo contactRepo;
+    private final UserRepo userRepo;
 
-    public InteractionServiceImpl(InteractionRepo interactionRepo) {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public InteractionServiceImpl(
+            InteractionRepo interactionRepo,
+            ContactRepo contactRepo,
+            UserRepo userRepo) {
         this.interactionRepo = interactionRepo;
+        this.contactRepo = contactRepo;
+        this.userRepo = userRepo;
     }
 
     @Override
-    public void saveInteraction(Interaction interaction) {
-        interactionRepo.save(interaction);
+    public Interaction createInteraction(Interaction interaction) {
+
+        if (interaction.getContact() == null || interaction.getContact().getId() == null) {
+            throw new IllegalArgumentException("Contact ID must be provided when creating an interaction");
+        }
+        if (interaction.getUser() == null || interaction.getUser().getId() == null) {
+            throw new IllegalArgumentException("User ID must be provided when creating an interaction");
+        }
+
+        Contact contactRef = entityManager.getReference(Contact.class, interaction.getContact().getId());
+        User userRef = entityManager.getReference(User.class, interaction.getUser().getId());
+
+        interaction.setContact(contactRef);
+        interaction.setUser(userRef);
+        return interactionRepo.save(interaction);
+    }
+
+    @Override
+    public Interaction addInteraction(Long contactId, String username, InteractionType type, String description) {
+        Contact contact = contactRepo.findById(contactId)
+                .orElseThrow(() -> new RuntimeException("Contact not found"));
+        User user = userRepo.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User: " + username + " not found"));
+
+        Interaction interaction = new Interaction();
+        interaction.setContact(contact);
+        interaction.setUser(user);
+        interaction.setType(type);
+        interaction.setDescription(description);
+        interaction.setDate(LocalDateTime.now());
+
+        return interactionRepo.save(interaction);
     }
 
     @Override
@@ -36,8 +86,11 @@ public class InteractionServiceImpl implements InteractionService{
     }
 
     @Override
-    public List<Interaction> findAllInteractionsWithContactId(Integer contactId) {
-        return List.of();
+    public Page<Interaction> findByContactId(Long contactId, int pageNumber, int pageSize) {
+
+        PageRequest pageRequest = PageRequest.of(pageNumber, pageSize);
+
+        return interactionRepo.findAllInteractionsByContact_Id(contactId, pageRequest);
     }
 
     @Override
