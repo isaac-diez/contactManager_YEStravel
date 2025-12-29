ba63f5300307790aebf44a0e6bce34df09eebfcd
isaac diez <isaac.diez@proton.me>
2025-12-29 19:44:20 +0100

    feat(service): created ContactImportService interface and implementation ContactImportServiceImpl

---
+++ src/main/java/com/yestravel/contactsmanager/service/ContactImportService.java
@@ -0,0 +1,9 @@
+package com.yestravel.contactsmanager.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface ContactImportService {
+    public void importContacts(MultipartFile file) throws IOException;
+}
