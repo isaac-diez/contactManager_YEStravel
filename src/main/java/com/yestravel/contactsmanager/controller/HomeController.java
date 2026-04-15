72737c74b510891febe711e1f263be950f6d255e
isaac diez <isaac.diez@proton.me>
2026-04-15 19:10:49 +0200

    refactor(controller): added /health to keep app alive

--- src/main/java/com/yestravel/contactsmanager/controller/HomeController.java
+++ src/main/java/com/yestravel/contactsmanager/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.yestravel.contactsmanager.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -9,5 +10,9 @@ public class HomeController {
     @GetMapping({"/", ""})
     public String rootRedirect() {
         return "redirect:/contacts";
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {return ResponseEntity.ok("OK");
     }
 }
